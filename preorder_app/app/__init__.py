356d58ab0abce83f398c22528f69091dde42fdf9
shaunakmohotra-byte <shaunakmohotra@tagoreint.com>
2026-02-01 12:29:31 +0530

    Update __init__.py

--- preorder_app/app/__init__.py
+++ preorder_app/app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
-    app.secret_key = os.environ.get('PREORDER_SECRET', 'Shaunak')
+    app.secret_key = os.environ.get('PREORDER_SECRET', 'asdhbasdiuabdquwubddbaijsrjasbisdbasdbasdiuiusadubausbd')
     # register blueprints
     from .routes import bp as main_bp
     from .auth import bp as auth_bp
