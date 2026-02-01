ce30f5ada55c8df399b05956d2d80f97b50f97f9
shaunakmohotra-byte <shaunakmohotra@tagoreint.com>
2026-02-01 12:47:37 +0530

    Update __init__.py

--- preorder_app/app/__init__.py
+++ preorder_app/app/__init__.py
@@ -4,6 +4,12 @@ def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
     app.secret_key = os.environ.get('PREORDER_SECRET', 'asdhbasdiuabdquwubddbaijsrjasbisdbasdbasdiuiusadubausbd')
     # register blueprints
+    def create_app():
+    app = Flask(__name__)
+    # DO NOT FORGET THIS LINE
+    app.config['SECRET_KEY'] = 'your-secret-key-here' 
+    
+    # ... rest of your code
     from .routes import bp as main_bp
     from .auth import bp as auth_bp
     from .admin import bp as admin_bp
