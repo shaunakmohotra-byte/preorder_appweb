a7cada096aef87da243630696c82e54130b4b477
shaunakmohotra-byte <shaunakmohotra@tagoreint.com>
2026-02-02 17:58:19 +0530

    Create email_utils.py

---
+++ preorder_app/app/email_utils.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+EMAIL_ADDRESS = "yourprojectemail@gmail.com"
+EMAIL_PASSWORD = "YOUR_APP_PASSWORD"   # NOT normal password
+
+def send_order_email(to_email, subject, body):
+    try:
+        msg = MIMEMultipart()
+        msg["From"] = EMAIL_ADDRESS
+        msg["To"] = to_email
+        msg["Subject"] = subject
+
+        msg.attach(MIMEText(body, "plain"))
+
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.send_message(msg)
+        server.quit()
+
+        return True
+
+    except Exception as e:
+        print("EMAIL ERROR:", e)
+        return False
